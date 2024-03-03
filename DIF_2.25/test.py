830292e9d3a4e140846fefec55938ee2f8676543
Dear-Sloth <145642003+Dear-Sloth@users.noreply.github.com>
2024-03-03 09:58:33 +0800

    Update test.py

--- DIF_2.25/test.py
+++ DIF_2.25/test.py
@@ -14,7 +14,7 @@ for dataset_name,num_vars in zip(["ETTh1"],[7]):
             num += 1
             cmd = f'python -u main_ddpm.py --seq_len {seq_len} --pred_len {pred_len} --dataset_name {dataset_name} \
             --features M --num_vars {num_vars} --pretrain_epochs 0 \
-            --train_epochs 100 --batch_size 64 --test_batch_size 64 --devices 0 --conditional False'
+            --train_epochs 100 --batch_size 64 --test_batch_size 64 --devices 0 --conditional True'
             try:
                 print(num)
                 subprocess.run(cmd, check=True, shell=True)
